937397da219ee0f7c00dea7a50ed57edc1dddea6
gsartorello <48076534+gsartorello@users.noreply.github.com>
2022-02-03 13:49:02 -0500

    Cleanup

--- standalone_stage_ui.py
+++ standalone_stage_ui.py
@@ -5,45 +5,14 @@ Control HLD-117 stage only
 Created 2022-Feb-03
 '''
 
-import os
-import platform
 import sys
-import matplotlib as mpl
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import rcParams
-import time
-from timeit import default_timer as timer, timeit
-# from experiment.defaults import *
 import experiment.defaults as defaults
-from experiment.routines_multithread import experiment
-from experiment.laser_windows import (laserInitializer,
-                                      laserSettingWindow,
-                                      laserStartupDialog)
 from experiment.stage_windows import (stageInitializer,
                                       stageMotionWindow,
                                       stageStartupDialog)
-from ui.plot_widgets import mplCanvas
-# from instruments.mircat import laser
-from instruments.ni_daq import MultiChannelAnalogInput as MultiAI
-from PyQt5.QtCore import Qt
-from PyQt5.QtCore import QObject, QThread, pyqtSignal
-from PyQt5.QtGui import QIntValidator, QIcon, QFont, QWindow
-from PyQt5.QtWidgets import (QAction,
-                             QApplication,
-                             QDesktopWidget,
-                             QDialog,
-                             QFileDialog,
-                             QGridLayout,
-                             QLabel,
-                             QLineEdit,
-                             QMainWindow,
-                             QMessageBox,
-                             QPushButton,
-                             QWidget,
-                             QSizePolicy,
-                             QTextEdit,
-                             QVBoxLayout)
+from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import (QApplication,
+                             QMainWindow)
 
 class mainWindow(QMainWindow):
     '''Dummy application window.'''
@@ -65,8 +34,6 @@ class mainWindow(QMainWindow):
         startupDialog2 = stageStartupDialog() # Closes when startup finishes
         self.stageWorker.stageInitialized.connect(lambda: startupDialog2.done(0))
         startupDialog2.exec()
-        ### Create GUI
-        # self.make_gui()
         ### Create window with stage motion controls
         self.stageMotionWindow = stageMotionWindow(self)
         ### Show stage motions control window
