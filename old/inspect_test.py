3a0920021c9c2cea423a6f6667ec8172b47ba86b
gsartorello <48076534+gsartorello@users.noreply.github.com>
2022-09-07 12:18:15 -0400

    File cleanup

--- old/inspect_test.py
+++
@@ -1,8 +0,0 @@
-import inspect
-import os
-
-print(inspect.currentframe())
-print(inspect.getfile(inspect.currentframe()))
-print(os.path.split(inspect.getfile(inspect.currentframe()))[0])
-print(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
-print(os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0])))
